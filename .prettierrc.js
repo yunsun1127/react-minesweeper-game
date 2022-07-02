e4acafd3fecfaa6c3b24c291f0e2ba5bad34b005
yunseon <64072104+yunsun1127@users.noreply.github.com>
2022-07-03 04:26:03 +0900

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    tabWidth: 2,
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Initial project</h1>
+}
+
+export default App
